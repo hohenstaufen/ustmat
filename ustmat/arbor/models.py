0592d4be884eab5d33c1049e65bc4e780c7f730f
Alberto Buratti <alberthohenstaufen@gmail.com>
2012-06-02 13:49:04 +0200

    added shape model

--- ustmat/arbor/models.py
+++ ustmat/arbor/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Shape(models.Model):
+    gid = models.IntegerField(primary_key=True)
+    address_po = models.IntegerField(null=True, blank=True)
+    address_fu = models.CharField(max_length=131, blank=True)
+    objectid = models.DecimalField(null=True, max_digits=10, decimal_places=0, blank=True)
+    struct_id = models.CharField(max_length=20, blank=True)
+    common_nam = models.CharField(max_length=254, blank=True)
+    botanical_ = models.CharField(max_length=254, blank=True)
+    diameter_b = models.DecimalField(null=True, max_digits=10, decimal_places=0, blank=True)
+    tree_posit = models.DecimalField(null=True, max_digits=10, decimal_places=0, blank=True)
+    geom = models.PointField(srid=0, null=True, blank=True)
+    objects = models.GeoManager()
+    class Meta:
+        db_table = u'stgd'
